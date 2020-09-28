60af5259cdae70a080dd01cb16a1c596607506ad
vineshc <vinesh.c@brillio.com>
2020-09-28 11:56:41 +0530

    Added scripts for Creating Opportunity

--- src/test/java/com/rabit/tests/salesforce/LoginAndCreateAnOpportunity.java
+++ src/test/java/com/rabit/tests/salesforce/LoginAndCreateAnOpportunity.java
@@ -3,9 +3,14 @@
  */
 package com.rabit.tests.salesforce;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Random;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 import com.rabit.util.SuperTestNG;
@@ -19,7 +24,7 @@ public class LoginAndCreateAnOpportunity extends SuperTestNG {
 	private static final int RANDOM_STRING_LENGTH = 10;
 	
 	@Test()
-	public void testAccount() throws InterruptedException {
+	public void testOpportunities() throws InterruptedException {
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//div[@id='username_container']/input")).clear();
 	    driver.findElement(By.xpath("//div[@id='username_container']/input")).sendKeys("autorabit@testing.com");
@@ -31,32 +36,50 @@ public class LoginAndCreateAnOpportunity extends SuperTestNG {
 	    Thread.sleep(10000);
 	    driver.findElement(By.xpath("//button[@title='Show Navigation Menu']")).click();
 	    Thread.sleep(2000);
-	    driver.findElement(By.xpath("//a[@title='Accounts']")).click();
+	    driver.findElement(By.xpath("//a[@title='Opportunities']")).click();
 	    Thread.sleep(2000);
 	    driver.findElement(By.xpath("//a[@title='New']")).click();
 	    Thread.sleep(2000);
-	    driver.findElement(By.xpath("//span[contains(text(),'Account Name')]/../..//*[@class=' input'][@type='text']")).click();
+	    driver.findElement(By.xpath("//span[contains(text(),'Opportunity Name')]/../..//*[@class=' input'][@type='text']")).click();
 	    Thread.sleep(2000);
-	    driver.findElement(By.xpath("//span[contains(text(),'Account Name')]/../..//*[@class=' input'][@type='text']")).clear();
+	    driver.findElement(By.xpath("//span[contains(text(),'Opportunity Name')]/../..//*[@class=' input'][@type='text']")).clear();
 	    Thread.sleep(2000);
-	    String Account= "Account " + generateRandomString();
-	    driver.findElement(By.xpath("//span[contains(text(),'Account Name')]/../..//*[@class=' input'][@type='text']")).sendKeys(Account);
+	    String Opportunity= "Opportunity " + generateRandomString();
+	    driver.findElement(By.xpath("//span[contains(text(),'Opportunity Name')]/../..//*[@class=' input'][@type='text']")).sendKeys(Opportunity);
 	    Thread.sleep(2000);
+	    DateFormat df = new SimpleDateFormat("dd");
+	    Date dateobj = new Date();
+	    String dateToString = df.format(dateobj); 
+	    System.out.println(dateToString);
+	    driver.findElement(By.xpath("//label[@class='label inputLabel uiLabel-left form-element__label uiLabel']//span[contains(text(),'Close Date')]")).click();
+	    Thread.sleep(2000);
+	    driver.findElement(By.xpath("//td[@class='slds-is-selected uiDayInMonthCell']")).click();
+	    //driver.findElement(By.xpath("//span[contains(text(),'"+dateToString+"')]")).click();;
+	    Thread.sleep(2000);
+	    driver.findElement(By.xpath("//span[contains(text(),'Stage')]/../..//a[@class='select']")).click();
+	    Thread.sleep(2000);
+	    driver.findElement(By.linkText("Prospecting")).click();
+	    Thread.sleep(2000);  
 	    driver.findElement(By.xpath("//button[@title='Save']")).click();
 	    Thread.sleep(2000);
 	    driver.findElement(By.xpath("//div[6]/div/div/div/button/lightning-primitive-icon")).click();
-	    Thread.sleep(3000);
-	    driver.findElement(By.xpath("(//a[contains(text(),'Details')])[2]")).click();
+	    driver.navigate().refresh();
+	    Thread.sleep(20000);
+	    WebElement element = driver.findElement(By.cssSelector("a#detailTab__item"));
+	    JavascriptExecutor executor = (JavascriptExecutor)driver;
+	    executor.executeScript("arguments[0].click();", element);
+	   // driver.findElement(By.xpath("//a[@id='detailTab__item']")).submit();
+	    //driver.findElement(By.id("detailTab__item")).click();
 	    Thread.sleep(2000);
-	   String ActualAccountName= driver.findElement(By.xpath("//span[contains(text(),'Account Name')]/../..//lightning-formatted-text")).getText();
-	   System.out.println(ActualAccountName);
-	   if(ActualAccountName.equalsIgnoreCase(Account))
+	   String ActualOpportunityName= driver.findElement(By.xpath("//span[contains(text(),'Opportunity Name')]/../..//lightning-formatted-text")).getText();
+	   System.out.println(ActualOpportunityName);
+	   if(ActualOpportunityName.equalsIgnoreCase(Opportunity))
 	   {
-		   System.out.println("Account Created Successfully");
+		   System.out.println("Opportunity Created Successfully");
 	   }
 	   else
 	   {
-		   System.out.println("Account NOT Created Successfully");
+		   System.out.println("Opportunity NOT Created Successfully");
 	   }
 	   
 	}
